883da69f8aba3d14625a437e19cfec5bb43a7b55
Tommi <akuankka75@gmail.com>
2021-05-07 17:34:43 +0300

    2.10, puhluettelo tehty

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -6,16 +6,39 @@ const RenderPhonebook = ({ filterText, persons }) => {
     <ul>
       {persons
         .filter(person => person.name.includes(filterText))
-        .map((person, i) => <RenderUserLi key={i} person={person} />)}
+        .map((person, i) => <RenderPerson key={i} person={person} />)}
     </ul>
   )
 }
+const RenderPerson = ({ person }) => <li>{person.name} {person.phonenumber}</li>
 
-const RenderUserLi = ({ person }) => <li>{person.name} {person.phonenumber}</li>
+const Filter = (props) => (
+    <div>
+      {props.text} <input value={props.inputText} onChange={props.onChange}></input>
+    </div>
+  )
 
+const InputForm = ({inputTypes}) => (
+    <form>
+      {inputTypes.map((input, i) => <NewInput key={i} inputInfo = {input}/>)}
+    </form>
+  )
+
+const NewInput = ({inputInfo}) => (
+  <>
+  {inputInfo.inputName}: <input value={inputInfo.inputValue} placeholder={inputInfo.placeHolder} onChange={inputInfo.onChange} /><br />
+  </>
+  )
+
+const NewButton = ({buttonInfo}) => (
+    <>
+    <button type={buttonInfo.type} onClick={buttonInfo.onClick}>{buttonInfo.text}</button>
+    </>
+  )
 
 
 const App = () => {
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', phonenumber: '05050' },
     { name: 'Ada Lovelace', phonenumber: '39-44-5323523' },
@@ -25,7 +48,6 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
-  const [showAll, setShowAll] = useState(true)
 
   const handleNewUser = (event) => {
     setNewName(event.target.value)
@@ -36,7 +58,6 @@ const App = () => {
   const handleNewFilter = (event) => {
     setFilterText(event.target.value)
   }
-
 
   const addNewUser = (event) => {
     event.preventDefault()
@@ -61,31 +82,22 @@ const App = () => {
     return false;
   }
 
-
+  const inputs = [
+    {inputName : 'name', inputValue : newName, placeHolder: 'Enter new name', onChange : handleNewUser },
+    {inputName : 'phone', inputValue : newNumber, placeHolder: 'Enter phonenumber', onChange : handleNewPhonenumber }  
+  ]
 
   return (
-    <div>
+    <div className="phonebook">
       <h2>Phonebook</h2>
-      <div>
-        filter shown with: <input value={filterText} onChange={handleNewFilter}></input>
-      </div>
-      <form>
-        <div>
-          <h2>Add new</h2>
-          name: <input value={newName} placeholder="enter new name" onChange={handleNewUser} /><br />
-          phone: <input value={newNumber} placeholder="enter phonenumber" onChange={handleNewPhonenumber} />
-        </div>
-        <div>
-          <button type="submit" onClick={addNewUser}>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <>
+        <Filter text="Filter shown with" inputText={filterText} onChange={handleNewFilter}/>
+      <h3>Add a new</h3>
+        <InputForm inputTypes={inputs}/>
+        <NewButton buttonInfo={{type : "submit", text : "add", onClick : addNewUser }}/>
+      <h3>Numbers</h3>
         <RenderPhonebook filterText={filterText} persons={persons} />
-      </>
     </div>
   )
-
 }
 
 export default App
