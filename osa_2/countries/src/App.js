5c040b829b0f38bdb4df5d3ba942f83fd4d16784
Tommi <akuankka75@gmail.com>
2021-05-08 19:40:37 +0300

    2.13

--- osa_2/countries/src/App.js
+++ osa_2/countries/src/App.js
@@ -5,16 +5,16 @@ const SearchBox = (props) => (
   <>{props.text} <input id={props.id} value={props.value} placeholder={props.placeholder} onChange={props.onChange} /></>
 )
 
-
 const DisplayCountryInfo = (props) => {
   let searchedCountries = []
-  console.log("hey", props.handleClick)
-
   props.countries
     .filter(country => country.name.toLowerCase().includes(props.filterText.toLowerCase()))
     .map((country) => (searchedCountries.push(country)))
 
   let res = searchedCountries.length
+
+  if (res === 0)
+    return <></>
 
   if (res >= 10) {
     return <div>
@@ -30,56 +30,62 @@ const DisplayCountryInfo = (props) => {
 
   return (
     <div>
-      {searchedCountries.map((country) => <SingleCountry key={country.numericCode} country={country} />)}
+      <SingleCountry key={searchedCountries[0].numericCode} country={searchedCountries[0]} />
     </div>
   )
 }
 
-const SingleCountry = (props) => (
-  <div className="singleCountry">
-    <h1>{props.country.name}</h1>
+const SingleCountry = (props) => {
+  console.log("singleCountry")
+  return (
+    <div className="singleCountry">
+      <h1>{props.country.name}</h1>
       Capital {props.country.capital} <br />
       Population {props.country.population}
-    <h2>Languages</h2>
-    <ul>
-      {props.country.languages
-        .map((language, i) =>
-          <li key={i}> {language.name}</li>)}
-    </ul>
-    <img src={props.country.flag} alt={props.country.name}></img>
-  </div>
-)
+      <h2>Languages</h2>
+      <ul>
+        {props.country.languages
+          .map((language, i) =>
+            <li key={i}> {language.name}</li>)}
+      </ul>
+      <img src={props.country.flag} alt={props.country.name}></img>
+    </div>
+
+  )
+}
 
 const FilteredCountryList = (props) => (
   <>{props.country.name} <button value={props.country.name} onClick={() => props.handleClick(props.country.name)}>show</button><br /></>
 )
 
 
-
-
 const App = () => {
 
   const [searched, newSearched] = useState('')
   const [countries, setCountries] = useState([])
+  const [oldCountry, newCountry] = useState(false)
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
+        console.log("effect axios")
         setCountries(response.data)
       })
   }, [])
+
+
+
 
   const handleSearchBox = (event) => newSearched(event.target.value)
   const handleClick = countryName => {
     newSearched(countryName)
   }
 
-
   return (
     <div>
-      <SearchBox id = "countrySearchBox" text='find countries' placeholder="Enter a country" onChange={handleSearchBox} value={searched} />
-      <DisplayCountryInfo filterText={searched} countries={countries} handleClick={handleClick} />
+      <SearchBox id="countrySearchBox" text='find countries' placeholder="Enter a country" onChange={handleSearchBox} value={searched} />
+      <DisplayCountryInfo filterText={searched} countries={countries} handleClick={handleClick}/>
     </div>
   )
 }
