e1b23cf2ccaad708bec95793d189f2fa54efcbdd
yellowpasta <78909617+yellowpasta@users.noreply.github.com>
2021-05-26 10:45:34 +0300

    Delete osa_4 directory

--- osa_4/tests/dummy.test.js
+++
@@ -1,7 +0,0 @@
-const dummyTest = require('../utils/list_helper').dummy
-
-test('dummy returns one', () => {
-    const blogs = []
-    const result = dummyTest(blogs)
-    expect(result).toBe(1)
-})
--- osa_4/utils/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
-
-module.exports = {
-    PORT,
-    MONGODB_URI
-}
