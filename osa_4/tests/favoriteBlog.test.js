a2cef943a23daba6f080d60e994f28ced99614b6
Tommi <akuankka75@gmail.com>
2021-05-26 08:34:28 +0300

    done 4.1 - 4.7

--- osa_4/tests/favoriteBlog.test.js
+++ osa_4/tests/favoriteBlog.test.js
@@ -71,27 +71,11 @@ describe('Testing favorite blog', () => {
     })
     test('of list with one blog', () => {
         const result = favoriteBlog(listWithOneBlog)
-        expect(result).toEqual(
-            {
-              _id: '5a422aa71b54a676234d17f8',
-              title: 'Go To Statement Considered Harmful',
-              author: 'Edsger W. Dijkstra',
-              url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-              likes: 2,
-              __v: 0
-            }
-          )
+        expect(result).toEqual(listWithOneBlog[0])
     })
     test('of list with many blog', () => {
         const result = favoriteBlog(listWithManyBlogs)
-        expect(result).toEqual({
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
-            __v: 0
-          })
+        expect(result).toEqual(listWithManyBlogs[2])
     })
     
 })
