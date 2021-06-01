9e2ebee34c0aca9b5d0fc7077714001db79a9f34
Tommi <akuankka75@gmail.com>
2021-06-01 10:25:42 +0300

    2.14

--- osa_2/countries/src/App.js
+++ osa_2/countries/src/App.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
@@ -27,13 +26,12 @@ const DisplayCountryInfo = (props) => {
 
   return (
     <div>
-      <SingleCountry key={props.countries[0].numericCode} country={props.countries[0]} />
+      <SingleCountry key={props.countries[0].numericCode} country={props.countries[0]} weatherData={props.weatherData} />
     </div>
   )
 }
 
 const SingleCountry = (props) => {
-  console.log("singleCountry")
   return (
     <div className="singleCountry">
       <h1>{props.country.name}</h1>
@@ -46,6 +44,7 @@ const SingleCountry = (props) => {
             <li key={i}> {language.name}</li>)}
       </ul>
       <img src={props.country.flag} alt={props.country.name}></img>
+      <DisplayWeather weatherData = {props.weatherData}/>
     </div>
 
   )
@@ -55,6 +54,16 @@ const FilteredCountryList = (props) => (
   <>{props.country.name} <button value={props.country.name} onClick={() => props.handleClick(props.country.name)}>show</button><br /></>
 )
 
+const DisplayWeather = ({weatherData}) => {
+  return(
+    <div>
+      <h1>Weather in {weatherData.location.name}</h1>
+      <p><span style={{fontWeight:"bold"}}>Temperature:</span> {weatherData.current.temperature} Celcius</p><br/>
+      <img id="weatherIcon" src={weatherData.current.weather_icons[0]} alt={weatherData.current.weather_descriptions}/><br/>
+      <p><span style={{fontWeight:"bold"}}>Wind:</span> {weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p>
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -62,12 +71,18 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [displayedCountries, setDisplayedCountries] = useState([])
   const [weatherCountry, setWeatherCountry] = useState('')
+  const [weatherData, setWeatherData] = useState({
+    location: {name: ""},
+    current: {temperature: "",weather_icons: [],
+    weather_descriptions: [],
+    wind_dir: ""
+    }
+  })
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log("effect axios")
         setCountries(response.data)
       })
   }, [])
@@ -87,25 +102,26 @@ const App = () => {
       } 
   }, [searched])
 
+  
   useEffect(() => {
     if(weatherCountry){
-    const address = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${weatherCountry.capital}`
-      console.log("logging weather", address)
+      const address = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_KEY}&query=${weatherCountry.capital}&units=m`
+      console.log("Weather api request was done")
+      axios.get(address)
+        .then(response => {
+          setWeatherData(response.data)
+        })
     }
   },[weatherCountry])
 
-
-
-
   const handleSearchBox = (event) => newSearched(event.target.value)
-  const handleClick = countryName => {
-    newSearched(countryName)
-  }
+  const handleClick = countryName => newSearched(countryName)
+  
 
   return (
     <div>
       <SearchBox id="countrySearchBox" text='find countries' placeholder="Enter a country" onChange={handleSearchBox} value={searched} />
-      <DisplayCountryInfo filterText={searched} countries={displayedCountries} handleClick={handleClick}/>
+      <DisplayCountryInfo filterText={searched} countries={displayedCountries} handleClick={handleClick} weatherData={weatherData}/>
     </div>
   )
 }
