b3c33196ff29ba02763522aae21c3a13db68e003
Tommi <akuankka75@gmail.com>
2021-06-02 10:45:46 +0300

    5.2

--- osa_5/bloglist-frontend/src/App.js
+++ osa_5/bloglist-frontend/src/App.js
@@ -8,38 +8,47 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState(undefined)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [notificationMessage, setNotificationMessage] = useState(null)
 
-  const ErrorNotification = ({message}) => {
+  const ErrorNotification = ({ message }) => {
     return (
       <div>
-        <h1 style={{color:"red"}}>{message}</h1>
+        <h1 style={{ color: "red" }}>{message}</h1>
       </div>
     )
   }
 
-  const loggedOutForm= () => {
+  const Notification = ({ message }) => {
     return (
-    <form onSubmit={handleLogin}>
       <div>
-      <h2>Log in to application</h2>
+        <h1 style={{ color: "green" }}>{message}</h1>
+      </div>
+    )
+  }
+
+  const loggedOutForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          <h2>Log in to application</h2>
         username
                 <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)} />
-      </div>
-      <div>
-        password
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password
                 <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)} />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     )
   }
 
@@ -47,7 +56,7 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />)}
       </div>
@@ -57,18 +66,28 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJson = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJson) {
+      const user = JSON.parse(loggedUserJson)
+      setUser(user)
+    }
+
   }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
-      const user = await loginService.login({username, password})
+      const user = await loginService.login({ username, password })
       setUser(user)
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
       setUsername('')
       setPassword('')
-    } catch(exception) {
+    } catch (exception) {
       setErrorMessage('Wrong credentials')
       setTimeout(() => {
         setErrorMessage(null)
@@ -76,16 +95,24 @@ const App = () => {
     }
   }
 
+  const handleLogout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+    setNotificationMessage('Logged out successfully')
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 2000)
+  }
 
-    
   return (
     <div>
-      <ErrorNotification message={errorMessage}/>
+      <ErrorNotification message={errorMessage} />
+      <Notification message={notificationMessage} />
       {user === null && loggedOutForm()}
       {user !== null && loggedInForm()}
     </div>
   )
 }
-
 
 export default App
