5bd34568f755585c725aa17c48ea371c047ff3bf
Tommi <akuankka75@gmail.com>
2021-06-03 10:20:55 +0300

    5.7

--- osa_5/bloglist-frontend/src/components/Blog.js
+++ osa_5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,42 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonStyle = {
+    background:'none',
+    borderRadius:'5px',
+    borderWidth:'1px'
+  }
+
+  const [viewBlog, setViewBlog] = useState(false)
+  const showWhenVisible = {display: viewBlog ? '' : 'none'}
+  const hideWhenVisible = {display: viewBlog ? 'none' : ''}
+
+  const toggleViewBlog = () => {
+    setViewBlog(!viewBlog)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible} >
+        {blog.title} {blog.author} <button type="button" style={buttonStyle} onClick={toggleViewBlog}>View</button>
+      </div>  
+      <div style={showWhenVisible}>
+        <h3>{blog.title}</h3>
+        By author: {blog.author}<br/>
+        Url: {blog.url}<br/>
+        Likes: {blog.likes} 
+        <button type="button" style={buttonStyle} onClick={toggleViewBlog}>Hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
