7f78febf93146b15e88cb982282de484e815a136
Tommi <akuankka75@gmail.com>
2021-06-19 16:10:38 +0300

    8.8 - 8.11

---
+++ osa_8/library-frontend/src/components/Authors.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import { useMutation, useQuery } from '@apollo/client'
+import { EDIT_AUTHOR, GET_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const [authors, setAuthors] = useState([])
+  const [name, setName] = useState('')
+  const [setBornTo, setBornYear] = useState('')
+  const result  = useQuery(GET_AUTHORS)
+
+  useEffect(() => {
+    if (result.data){
+      setAuthors(result.data.allAuthors)
+    }
+  }, [result])
+
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: GET_AUTHORS } ]
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+    editAuthor( { variables : { name, setBornTo } })
+
+    setName('')
+    setBornYear('')
+
+  }
+
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div>
+        <h2>Set birthyear</h2>
+        <form onSubmit={submit}>
+          <div>
+            name <input type="text" value={name} onChange={({ target }) => setName(target.value)}/>
+          </div>
+          <div>
+            born <input type="number" value={setBornTo} onChange={({ target }) => setBornYear(parseInt(target.value))}/>
+          </div>
+          <button type="submit">update author</button>
+
+        </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default Authors
---
+++ osa_8/library-frontend/src/components/Books.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_BOOKS } from '../queries'
+
+const Books = (props) => {
+
+  const [books, setBooks] = useState([])
+  const result = useQuery(GET_BOOKS)
+
+  useEffect(() => {
+    if (result.data) {
+      setBooks(result.data.allBooks)
+    }
+  }, [result])
+
+  if (!props.show) {
+    return null
+  }
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+            </th>
+            <th>
+              published
+            </th>
+          </tr>
+          {books.map(a =>
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
