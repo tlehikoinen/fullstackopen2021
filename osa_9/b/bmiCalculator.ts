7f0ba44e58fd789a231b19f2b455dd1ed754fe27
Tommi <akuankka75@gmail.com>
2021-07-26 17:02:38 +0300

    9.5

--- osa_9/b/bmiCalculator.ts
+++ osa_9/b/bmiCalculator.ts
@@ -1,11 +1,12 @@
-const calculateBmi = (heightCm: number, weightKg: number):string => {
+export const calculateBmi = (heightCm: number, weightKg: number):string => {
     const bmiValue =  weightKg / (heightCm/100) ** 2
     console.log(bmiValue)
 
     if (bmiValue < 18.5) return 'Underweight'
     if (bmiValue >= 18.5 && bmiValue < 25 ) return 'Normal (healthy weight)'
     if (bmiValue >= 25 && bmiValue < 30) return 'Overweight'
-    return 'Obese (heavily overweight)'
+    if (bmiValue >=30) return 'Obese (heavily overweight)'
+    return 'Malformatted parameters'
 }
 
 const height: number = Number(process.argv[2]);
--- osa_9/b/index.ts
+++ osa_9/b/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 const app = express();
 
+import { calculateBmi } from './bmiCalculator'
+
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req,res) => {
+
+
+  const weight: number = Number(req.query.weight);
+  const height: number = Number(req.query.height);
+  const bmi = calculateBmi(height, weight);
+
+  const result = bmi === 'Malformatted parameters' 
+  ? { error: 'Malformatted parameters'} 
+  : {
+    weight,
+    height,
+    bmi
+  }
+  return res.send(result)
 });
 
 const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
