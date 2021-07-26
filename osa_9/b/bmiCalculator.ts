2f60f5c3b525e9be559143569991ed057857fd7c
Tommi <akuankka75@gmail.com>
2021-07-26 15:58:39 +0300

    9.4

--- osa_9/b/bmiCalculator.ts
+++ osa_9/b/bmiCalculator.ts
@@ -5,10 +5,10 @@ const calculateBmi = (heightCm: number, weightKg: number):string => {
     if (bmiValue < 18.5) return 'Underweight'
     if (bmiValue >= 18.5 && bmiValue < 25 ) return 'Normal (healthy weight)'
     if (bmiValue >= 25 && bmiValue < 30) return 'Overweight'
-    if (bmiValue >=30) return 'Obese (heavily overweight)'
+    return 'Obese (heavily overweight)'
 }
 
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
 
-console.log(calculateBmi(height, weight))
+console.log(calculateBmi(height, weight));
