94465133fc8b85ca936aababca1800824fdefe6d
Tommi <akuankka75@gmail.com>
2021-07-28 23:07:36 +0300

    9.19

--- osa_9/patientor-backend/src/types.ts
+++ osa_9/patientor-backend/src/types.ts
@@ -5,8 +5,6 @@ export enum Gender {
     Other = "other"
 }
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
 
 export interface PatientData {
     id: string;
@@ -26,3 +24,39 @@ export interface DiagnoseData {
     name: string;
     latin?: string;
 }
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseData['code']>;
+  }
+
+  export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+  
+  interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+  }
+
+  interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: { startDate: string, endDate: string}
+  }
+
+  interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: { date: string, criteria: string }
+  }
+
+  export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
